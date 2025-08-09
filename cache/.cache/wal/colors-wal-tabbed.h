647df1aac39280a9f938711a070d80f8255dea86
dizzi1222 <127579140+dizzi1222@users.noreply.github.com>
2025-08-09 08:51:59 -0400

    Delete cache/.cache/wal directory
    El cache se autogenera bobi

--- cache/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#1a0d1e";
-static const char* selfgcolor   = "#c5c2c6";
-static const char* normbgcolor  = "#92468B";
-static const char* normfgcolor  = "#c5c2c6";
-static const char* urgbgcolor   = "#8C3A90";
-static const char* urgfgcolor   = "#c5c2c6";
