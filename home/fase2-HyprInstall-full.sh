26384fcfc7e0106094062d67e158456787e516f7
dizzi1222 <diegosamuel042@gmail.com>
2026-01-26 21:49:36 -0400

    ✨ Actualizar texto de animación en script de Hyprland
    Cambia el texto mostrado en la animación de "Neo" a "Arch is so GOATed UwU" para
    personalizar la experiencia del usuario en el entorno de Hyprland.

--- home/fase2-HyprInstall-full.sh
+++ home/fase2-HyprInstall-full.sh
@@ -1613,15 +1613,12 @@ print_status "Aplicando Submodulos [NVIM]    ."
 echo "${BOLD}${CYAN}Paso 1: Clonando repositorios...${RESET}"
 # Verificar submodules
 git submodule update --init --recursive
-rm -rf Librezam retro-portfolio kimu-underground portfolio-terminal-dhardi
+rm -rf nvim
 
 # Recuperar cada submódulo
-git submodule update --init --recursive Librezam
-git submodule update --init --recursive retro-portfolio
-git submodule update --init --recursive kimu-underground
-git submodule update --init --recursive portfolio-terminal-dhardi
+git submodule update --init --recursive nvim
 
-echo "${BOLD}${CYAN}Paso 1: Clonando repositorios...${RESET}"
+echo "${BOLD}${CYAN}Paso 2: Corrigiendo el branch main...${RESET}"
 cd  nvim/.config/nvim  && git checkout main
 cd  ../../../
 
