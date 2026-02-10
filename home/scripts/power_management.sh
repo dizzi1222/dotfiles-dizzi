60de06508363b7dca9a6db6b381d9f65c2bf5041
dizzi1222 <diegosamuel042@gmail.com>
2026-02-09 21:58:25 -0400

    chore: add alternative init system option and update config files
    Add commented alternative init system option in GRUB configuration
    and update binary files for Hypr installation, power management,
    text animation content, and dconf user settings

--- home/scripts/power_management.sh
+++ home/scripts/power_management.sh
@@ -2,7 +2,7 @@
 # CONFIG de ZENITIES- THEMES - hayyaoe
 # #######################################################################################
 
-CHOICE=$(printf "\n\n\n\n󰒲\n" | rofi -dmenu -replace -config ~/.config/rofi/config-power.rasi)
+CHOICE=$(printf "\n\n\n\n\n󰒲" | rofi -dmenu -replace -config ~/.config/rofi/config-power.rasi)
 
 case "$CHOICE" in
 "")
@@ -24,19 +24,22 @@ case "$CHOICE" in
   ;;
 "")
   cd /$HOME
-  systemctl suspend
+  sync # Fuerza escritura a disco
+  sleep 0.5
+  systemctl --force --force suspend # o usa sleep
   ;;
 "󰒲")
   # hibernar
-  cd /$HOME
-  sync # Fuerza escritura a disco
-  sleep 0.5
   # Hibernation configurado en GRUB: resume=/swapfile resume_offset=18472960
+  # sudo systemctl hibernate --force --force
   sudo systemctl hibernate
+  # Verificar si la hibernación está correctamente configurada
+  if ! grep -q "resume=" /proc/cmdline || ! swapon --show | grep -q "/"; then
+    notify-send "⚠️ NO PUEDES HIBERNAR!" "Falta configuración de SWAP o parámetros de resume en GRUB " -i dialog-warning -t 5000
+  fi
   ;;
 "")
   cd /$HOME
-  hyprctl dispatch exit
   if pgrep -x "niri" >/dev/null; then
     # Niri: mata el proceso principal (equivale a "exit")
     killall niri
@@ -44,9 +47,9 @@ case "$CHOICE" in
     sleep 2
     pkill -KILL niri # Por si no respondió al TERM
   fi
+  hyprctl dispatch --force --force exit
   ;;
 *)
   exit 1
   ;;
 esac
-
