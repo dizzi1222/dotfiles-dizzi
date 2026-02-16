7837769096b0673424b332dc8bf672ecf8d0a8dc
Diego Härdi <127579140+dizzi1222@users.noreply.github.com>
2026-02-16 01:35:03 -0400

    Actualizar fase2-HyprInstall-full.sh

--- home/fase2-HyprInstall-full.sh
+++ home/fase2-HyprInstall-full.sh
@@ -232,7 +232,7 @@ sudo pacman -S --needed --noconfirm \
   grim slurp wl-clipboard cliphist \
   brightnessctl playerctl pamixer \
   swaync hyprlock hypridle hyprpicker \
-  wofi fuzzel polkit-kde-agent udiskie \
+  wofi fuzzel polkit-kde-agent polkit-gnome udiskie \
   swww hyprpaper hyprshot \
   qt5-wayland qt6-wayland gtk-layer-shell
 
