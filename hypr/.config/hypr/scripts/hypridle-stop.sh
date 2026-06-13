2cfa52a4ae3b8f88a25e68f1ece4fcb40a8c7d18
dizzi1222 <diegosamuel042@gmail.com>
2026-06-13 13:28:53 -0400

    📝 Añadir credenciales de Jhon al archivo de anotaciones ✨ Actualizar mensaje en script de animación de texto a “Howdy my Sigma!”
    📝 actualizar archivo de configuración kdenliverc para reflejar cambios recientes
    
    ♻️ config: actualizar resolución predeterminada a 960x540 y limpiar kdockLayout en kdenliverc
    
    🔧 agregar layout predeterminado de Kdenlive en archivo de configuración
    
    🐛 Cambiar la ruta del script en el .desktop de Hypridle Stop para usar el script local correcto.

---
+++ hypr/.config/hypr/scripts/hypridle-stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pkill -STOP hypridle
+notify-send " Hypridle" "Detenido (congelado)" -i /home/diego/.local/share/icons/Hyprland_logo.png
--- hypr/.config/hypr/scripts/restart-hypridle.sh
+++ hypr/.config/hypr/scripts/restart-hypridle.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pkill -9 hypridle 2>/dev/null
 systemctl --user start hypridle
+notify-send " Hypridle" "Iniciado" -i /home/diego/.local/share/icons/Hyprland_logo.png
