419ef80fc200d3d450343ba502f5006b3e9941f2
dizzi1222 <diegosamuel042@gmail.com>
2026-07-14 14:34:42 -0400

    ✨ (settings.json): eliminar la entrada de favoritos para pokemmo ✨ (scripttext): actualizar el texto de la animación ✨ (dconf/user): actualizar archivo binario de configuración ✨ (Launcher.dxvk-cache): añadir caché para el lanzador de juegos ✨ (pes6.sh): añadir script para ejecutar PES6 ✨ (pokeone.sh): actualizar script para ejecutar PokeOne ✨ (stardew.sh): añadir script para ejecutar Stardew Valley ✨ (PokeOne.desktop): actualizar comando de ejecución para PokeOne ✨ (Stardew Valley.desktop): actualizar comando de ejecución para Stardew Valley ✨ (PES6.desktop): añadir archivo de escritorio para PES6 ✨ (icons/.stow-local-ignore): añadir archivo para ignorar íconos .ico ✨ (blasphemous.yml): añadir variable de entorno WINEDEBUG ✨ (cuphead.yml): añadir variable de entorno WINEDEBUG ✨ (dead-cells.yml): añadir variable de entorno WINEDEBUG ✨ (geometry-dash.yml): añadir variable de entorno WINEDEBUG ✨ (hades.yml): añadir variable de entorno WINEDEBUG ✨ (hollow-knight.yml): añadir variable de entorno WINEDEBUG ✨ (hollow-knight-silksong.yml): añadir variable de entorno WINEDEBUG ✨ (pokemmo.yml): añadir variable de entorno WINEDEBUG ✨ (pokeone.yml): añadir variable de entorno WINEDEBUG ✨ (pro-evolution-soccer-2010.yml): actualizar comando de ejecución para PES6 ✨ (stardew-valley.yml): añadir variable de entorno WINEDEBUG 🔧 (lutris.conf): ajustar dimensiones de la ventana de Lutris ✨ (apps.json): actualizar comandos de ejecución para juegos en Sunshine

---
+++ local/.local/bin/juegos/pes6.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export WINEDEBUG=-all
+export LUTRIS_SKIP_INIT=1
+wine "/home/diego/.wine/drive_c/Games/KONAMI/Pro Evolution Soccer 6/PES6.exe"
--- local/.local/bin/juegos/pokeone.sh
+++ local/.local/bin/juegos/pokeone.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+export WINEDEBUG=-all
+export LUTRIS_SKIP_INIT=1
 wine "/home/diego/.local/share/bottles/bottles/gaming/drive_c/Games/PokeOne/Launcher.exe"
